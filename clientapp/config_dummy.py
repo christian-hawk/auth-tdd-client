1102cff98bff849c71357ff395a8d940f4e88046
SafinWasi <6601566+SafinWasi@users.noreply.github.com>
2022-09-05 10:38:34 -0500

    Slight refactor

---
+++ clientapp/config_dummy.py
@@ -0,0 +1,27 @@
+CLIENT_ID = ""
+CLIENT_SECRET = ""
+CLIENT_AUTH_URI = "https://your-authorize-endpoint"
+TOKEN_URI = "https://your-token-endpoint"
+USERINFO_URI = "https://your-userinfo-endpoint"
+REDIRECT_URIS = [
+    'https://localhost:9090/oidc_callback'
+]
+ISSUER = "https://your-server's-fqdn"
+
+SERVER_META_URL = "https://your-openid-configuration-uri"
+
+# Token authentication method can be
+# client_secret_basic
+# client_secret_post
+# none
+
+SERVER_TOKEN_AUTH_METHOD = "client_secret_post"
+
+# for gluu
+ACR_VALUES = 'passport_saml'
+PRE_SELECTED_PROVIDER = True
+PRE_SELECTED_PROVIDER_ID = ''
+
+# SYSTEM SETTINGS
+# use with caution, unsecure requests, for develpment environments
+SSL_VERIFY = True
